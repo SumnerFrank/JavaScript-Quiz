ffc2407db87f5374d4c4dfc7a4f16f8e16451010
Franklin Sumner <sumnerfranklinm@gmail.com>
2022-11-23 08:50:48 -0500

    added score variable, moved questions to bottom to make them easier to find

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 //connect js to start button 
+var score = 0;
 var startButton = document.querySelector("#startButton")
+
+
+function startQuiz() {
+
+
+}
+
+startButton.addEventListener("click", startQuiz);
+
+for(var i=0; i < questions.length; i++); {
+    var response = window.prompt(questions[i].prompt);
+    if(response == questions[i].correct) {
+        score++;
+        alert("CORRECT!");
+    } else {
+        alert("incorrect :(");
+    }
+}
+
 var questions = [
     {
         message: "what is 1 + 1?", 
@@ -10,13 +30,20 @@ var questions = [
         message: "what is 2 + 2?", 
         choices: [2, 4, 6, 8], 
         correct: 4
-    }
+    },
+    {
+        message: "what is 2(1 + 1)?", 
+        choices: [2, 4, 6, 8], 
+        correct: 4
+    }, 
+    {
+        message: "what is 2(2 + 2)?", 
+        choices: [2, 4, 6, 8], 
+        correct: 8
+    },
+    {
+        message: "what is 3(1 + 1)?", 
+        choices: [2, 4, 6, 8], 
+        correct: 6
+    }, 
 ]
-
-
-function startQuiz() {
-
-
-}
-
-startButton.addEventListener("click", startQuiz);
